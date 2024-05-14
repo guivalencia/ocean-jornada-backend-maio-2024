b8fc8436dd640bbc2a3acd0b308ed6ac5041f0b9
Guilherme Valencia <57770213+guivalencia@users.noreply.github.com>
2024-05-14 17:30:43 +0000

    Enviando parametro ID

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
-
-app.get('/', function (req, res) {
-    res.send('<h1>Hello World</h1>')
+app.get('/oi', function (req, res) {
+  res.send('Olá Mundo!')
 })
 
-app.get('/oi', function (req, res) {
-    res.send('Olá mundo!')
+const itens = ['Rick', 'Morty', 'Summer']
+
+app.get('/item/:id', function(req, res){
+    //Acessamos o parametro de rota ID
+    const id = req.params.id
+
+    //Acessamos o item na lista usando o ID -1
+    const item = itens[id -1]
+
+    //Enviamos o item encontrado como resposta
+    res.send(item)
 })
 
 app.listen(3000)
