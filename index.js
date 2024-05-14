7fe16e29f49ab07849a8f43e85d3cd0bfc62c856
Guilherme Valencia <57770213+guivalencia@users.noreply.github.com>
2024-05-14 18:32:22 +0000

    Endpoint Update

--- index.js
+++ index.js
@@ -1,42 +1,68 @@
 const express = require('express')
 const app = express()
 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+// Desafio: criar uma rota [GET] /oi que retorna "Olá, mundo!"
 app.get('/oi', function (req, res) {
-  res.send('Olá Mundo!')
+  res.send('Olá, mundo!')
 })
 
-const itens = ['Rick', 'Morty', 'Summer']
+// Lista de Itens
+const itens = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+//              0               1              2
 
-app.get('/item/:id', function(req, res){
-    //Acessamos o parametro de rota ID
-    const id = req.params.id
-
-    //Acessamos o item na lista usando o ID -1
-    const item = itens[id -1]
-
-    //Enviamos o item encontrado como resposta
-    res.send(item)
+// Endpoint de Read All [GET] /item
+app.get('/item', function (req, res) {
+  res.send(itens)
 })
 
-//Sinalizamos que todo corpo de requisição
-//virá como JSON
+// Endpoint de Read By ID [GET] /item/:id
+app.get('/item/:id', function (req, res) {
+  // Acessamos o parâmetro de rota ID
+  const id = req.params.id
+
+  // Acessamos o item na lista usando o ID - 1
+  const item = itens[id - 1]
+
+  // Enviamos o item encontrado como resposta
+  res.send(item)
+})
+
+// Sinalizamos que todo corpo de requisição
+// virá como JSON
 app.use(express.json())
 
-//Endpoint de Create [POST] /item
-app.post('/item', function (req, res){
-    //Acessando o corpo da requisição
+// Endpoint de Create [POST] /item
+app.post('/item', function (req, res) {
+  // Acessamos o corpo da requisição
+  const body = req.body
+
+  // Acessar o item no corpo da requisição
+  const novoItem = body.nome
+
+  // Adicionar o novo item na lista
+  itens.push(novoItem)
+
+  // Enviar uma mensagem de sucesso
+  res.send('Item adicionado com sucesso: ' + novoItem)
+})
+
+//Endpoint de Update [PUT] /item/:id
+app.put('/item/:id', function (req, res){
+    //Acessar o ID do parametro de rota
+    const id = req.params.id
+    
+    //Acessar o item a ser atualizado, a partir do corpo da requisição
     const body = req.body
+    const atualizarItem = body.nome
 
-    //Acessar o item no corpo da requisição
-    const novoItem = body.nome
+    //Atualizar na lista o item recebido
+    itens[id - 1] = atualizarItem
 
-    //Adicionar o novo item na lista
-    itens.push(novoItem)
-
-    //Enviar uma mensagem de sucesso
-    res.send('Item adicionado com sucesso ' + novoItem)
-
-
+    res.send('Item atualizado com sucesso: ' + id + ', ' + atualizarItem)
 })
 
 app.listen(3000)
