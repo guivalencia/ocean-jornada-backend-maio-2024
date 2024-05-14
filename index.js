2f0abd8b5519a1f0ef4acce31d2aa74796cdf8af
Guilherme Valencia <57770213+guivalencia@users.noreply.github.com>
2024-05-14 17:55:26 +0000

    Criação Endpoint Create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+
 app.get('/oi', function (req, res) {
   res.send('Olá Mundo!')
 })
@@ -15,6 +16,27 @@ app.get('/item/:id', function(req, res){
 
     //Enviamos o item encontrado como resposta
     res.send(item)
+})
+
+//Sinalizamos que todo corpo de requisição
+//virá como JSON
+app.use(express.json())
+
+//Endpoint de Create [POST] /item
+app.post('/item', function (req, res){
+    //Acessando o corpo da requisição
+    const body = req.body
+
+    //Acessar o item no corpo da requisição
+    const novoItem = body.nome
+
+    //Adicionar o novo item na lista
+    itens.push(novoItem)
+
+    //Enviar uma mensagem de sucesso
+    res.send('Item adicionado com sucesso ' + novoItem)
+
+
 })
 
 app.listen(3000)
